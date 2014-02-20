fccbdbf94c875c22656be16e296a3317ada872c4
jeswsi <james@woodruffscientific.com>
2014-02-20 15:39:37 -0800

    fixed links and .php redirects for bluehost

--- index.php
+++ index.php
@@ -65,29 +65,29 @@
 	<div class="col-md-6">
 		<a class="black" href="products"><h2>Products</h2></a>
 		<ul>
-		<li><a href="prod/circuit">Pulsed power</a></li>
-		<li><a href="prod/magnets">Magnets</a></li>
-		<li><a href="prod/chambers">Confinement chambers</a></li>
-		<li><a href="prod/coda">CODAC</a></li>
-		<li><a href="prod/diagnostics">Diagnostics</a></li>
-		<li><a href="prod/device">Devices</a></li>
-		<li><a href="prod/hpc">Computers</a></li>
+		<li><a href="pulsedpower">Pulsed power</a></li>
+		<li><a href="magnets">Magnets</a></li>
+		<li><a href="chambers">Confinement chambers</a></li>
+		<li><a href="codac">CODAC</a></li>
+		<li><a href="diagnostics">Diagnostics</a></li>
+		<li><a href="devices">Devices</a></li>
+		<li><a href="computers">Computers</a></li>
 		</ul>
 	</div>
 	<div class="col-md-6">
 		<a class="black" href="services"><h2>Services</h2></a>
 		<ul>
-		<li><a href="http://woodruffscientific.com/wp/serv/physic">Physics</a></li>
-		<li><a href="http://woodruffscientific.com/wp/serv/engineering">Engineering</a></li>
-		<li><a href="http://woodruffscientific.com/wp/serv/purchase">Purchase</a></li>
-		<li><a href="http://woodruffscientific.com/wp/serv/fabtes">Fabrication / Integration</a></li>
-		<li><a href="http://woodruffscientific.com/wp/serv/installation-testing">Installation / Testing</a></li>
+		<li><a href="physics">Physics</a></li>
+		<li><a href="engineering">Engineering</a></li>
+		<li><a href="purchase">Purchase</a></li>
+		<li><a href="fabrication">Fabrication / Integration</a></li>
+		<li><a href="installation">Installation / Testing</a></li>
 		</ul>
 	</div>
 	<div class="col-md-12 center">
 	<h2>Our Mission</h2>
 	<p>
-Nuclear fusion powers our sun and all the stars in the night sky; if harnessed on earth, it could be the ultimate energy source: safe, clean, and with plentiful fuel. Commercial fusion energy will ultimately provide energy independence, national security, and new jobs in the emerging clean-tech market sector.  For the last decade, Woodruff Scientific has sought to accelerate the development of commercial fusion by tackling critical scientific problems in the <a href="http://science.energy.gov/fes/">US DOE Fusion Energy Sciences program</a>.  What we&#8217;ve built, we can now offer as <a href="prod">products.</a> What we&#8217;ve learned to do, we offer as <a href="serv">services</a>.
+Nuclear fusion powers our sun and all the stars in the night sky; if harnessed on earth, it could be the ultimate energy source: safe, clean, and with plentiful fuel. Commercial fusion energy will ultimately provide energy independence, national security, and new jobs in the emerging clean-tech market sector.  For the last decade, Woodruff Scientific has sought to accelerate the development of commercial fusion by tackling critical scientific problems in the <a href="http://science.energy.gov/fes/">US DOE Fusion Energy Sciences program</a>.  What we&#8217;ve built, we can now offer as <a href="products">products.</a> What we&#8217;ve learned to do, we offer as <a href="services">services</a>.
 	</p>
 	</div>
     </div> <!-- /container -->
