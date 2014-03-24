bf7bed3348fa3b1db1e1641213f9ade21e8427c4
jeswsi <james@woodruffscientific.com>
2014-03-24 13:17:12 -0700

    added news page

--- news.php
+++ news.php
@@ -5,8 +5,8 @@
 	$head = file_get_contents('header.html');
 	echo $head;	
 	?>
-  <meta name="description" content="">
-  <title>TITLE | Woodruff Scientific, Inc.</title>
+  <meta name="description" content="News and Updates at Woodruff Scientific, Inc.">
+  <title>News | Woodruff Scientific, Inc.</title>
   </head>
 
   <body>
@@ -18,12 +18,107 @@
 	echo $navbar;	
 	?>	
 	<ol class="breadcrumb">
-  		<li><a href="index">Home</a></li>
- 	 	<li><a href="#">Subsection</a></li>
-  		<li class="active">Current Page</li>
+  		<li><a href="/">Home</a></li>
+ 	 	<li><a href="company">Company</a></li>
+  		<li class="active">News</li>
 	</ol>
-	<h1>Title</h1>
-	<p>Paragraph</p>
+	<h1>News</h1>
+	<p>Get news updates via email: <a href="mailto:info@woodruffscientific.com">info@woodruffscientific.com</a></p>
+
+	<div class="col-md-10">
+	<h3>2014</h3>
+	<p><span class="text-muted">January</span><br />Associates newsletter sent out, planning for scientific computing ‘bootcamp’ begins (likely in June – announcement to follow).</p>
+
+
+	<h3>2013</h3>
+	<p><span class="text-muted">December 6</span><br />Physics point design for CT-based reactor and neutron source presented to IAEA CRP on compact neutron sources (WSI organized conference call).</p>
+	<p><span class="text-muted">November 11 - 15</span><br />Presentation of ITER VDE results at American Physical Society Divsion of Plasma Physics meeting in Denver, CO</p>
+	<p><span class="text-muted">September 27</span><br />Sam Schetterer presents his summer internship poster ‘Mesh optimization for 3D MHD codes’ to NASA Space Grant Workshop, held at the University of Washington.</p>
+	<p><span class="text-muted">September 25</span><br /> WSI welcomes Steven Diesburg as Design Engineer II. Steven will be helping to manage all the revamping of the website, and hold us to task with capability development.</p>
+	<p><span class="text-muted">September 21</span><br />HH coils shipped to LANL</p>
+	<p><span class="text-muted">September 3</span><br />WSI welcomes Myunghee Choi as associate.</p>
+	<p><span class="text-muted">June 24</span><br />New contract to LANL secured for design, and fab of Helmholtz coils.</p>
+	<p><span class="text-muted">June 24</span><br />New contract to USTC secured for MHD simulations of mirror plasmas.</p>
+	<p><span class="text-muted">June 16</span><br />WSI welcomes Sam Schetterer (BSc Physics) as this year’s NASA Space Grant Summer Intern.</p>
+	<p><span class="text-muted">May 20</span><br />WSI welcomes Tim Gray, Karsten McCollam, Nate Hicks, Chris Raastad, Carlos Romero-Talamas to newly started Associates Program.</p>
+	<p><span class="text-muted">May 8</span><br /><a href="http://burningplasma.org/web/fsff-whitepapers.html">White paper</a> published on possible future fusion facility, as part of the FESAC Future Facilities panel.</p>
+	<p><span class="text-muted">April 8</span><br />Invited talk: 2nd design point for compact fusion system at the University of Washington, Aeronautics and Astronautics Monday Plasma Physics Seminar</p>
+	<p><span class="text-muted">March</span><br />James Stuber completes ITER stray-light modeling contract onsite at the IO in Cadarache. One Plasma Physics and Controlled Fusion paper published, one Rev. Sci. Instrum. paper near to submittal.</p>
+	<p><span class="text-muted">February 14-17</span><br />WSI presents 2nd design point for costing analysis of compact fusion system and initial results from disruption mitigation studies at the Exploratory Plasma Research workshop in Fort Worth, Texas</p>
+
+
+	<h3>2012</h3>
+	<p><span class="text-muted">November 4-6</span><br />WSI presents at IAEA CRM in Vienna, Austria, outline of compact fusion neutron source for concept design review.</p>
+	<p><span class="text-muted">October 29 – November 2</span><br />WSI presents results on costing analysis for compact fusion systems at the American Physical Society DPP, Providence, Rhode Island</p>
+	<p><span class="text-muted">October 6</span><br />Contract to ORNL for disruption simulations won. </p>
+	<p><span class="text-muted">September 20</span><br />WSI receives invitation to IAEA CRP on compact neutron sources to present work ongoing on our compact fusion concept.</p>
+	<p><span class="text-muted">August</span><br />1st plasmas on STPX achieved. </p>
+	<p><span class="text-muted">July 31</span><br />FESAC Panel on MFE Priorities (except ITER Project, HEDLP and Basic Plasma Science) held its second meeting. WSI submitted short white paper on <a href="http://fire.pppl.gov/FESAC_WP_CT_market_Woodruff.pdf">Path to Market</a> based on longer <a href="http://fire.pppl.gov/FESAC_WP_Path2Mark_SWood.pdf">J. Fusion Energy paper</a>.</p>
+	<p><span class="text-muted">July</span><br />Installation of STPX completed 
+		<br /><a href="img/STPX.jpg"><img src="img/STPX-300x199.jpg" alt="STPX"></a> <a href="img/stpx_banks.jpg"><img src="img/stpx_banks_small.jpg" alt="STPX Banks"></a></p>
+	<p><span class="text-muted">June</span><br />Compact Fusion Systems Inc started as vehicle for IP protection of compact fusion energy systems. </p>
+	<p><span class="text-muted">March </span><br />WSI welcomes Nate Hicks as Physicist to assist with new and continuing contracts.</p>
+	<p><span class="text-muted">February</span><br />WSI wins ITER contract for optical diagnostic design. </p>
+	<p><span class="text-muted">February</span><br />WSI presents to NW Energy Angels.</p>
+
+	<h3>2011</h3>
+	<p><span class="text-muted">December 14</span><br /> ‘Path to Market’ presentation and white paper presented to the <a href="http://fire.pppl.gov/fpa_annual_meet.html#2011">Fusion Power Associates</a> on Capitol Hill, Washington DC (shortly after Steve Koonin). </p>
+	<p><span class="text-muted">October</span><br />WSI relocates to Wallingford. </p>
+	<p><span class="text-muted">September </span><br />WSI secures SAIC contract for simulations of stability of Field Reversed Configurations for the compressional heating experiment at AFRL. </p>
+	<p><span class="text-muted">September </span><br />Journal of Fusion Energy accepts paper for publication, titled ‘Path to market for compact modular fusion power cores’ </p>
+	<p><span class="text-muted">August</span><br />WSI starts work with Princeton Plasma Physics Laboratory NSTX project to update passive structure model.
+		<br /><a href="img/NSTX.jpg"><img src="img/NSTX-300x276.jpg" alt="NSTX equilibrium"></a></p>
+	<p><span class="text-muted">August</span><br />WSI shipped and received STPX device at FAMU. </p>
+	<p><span class="text-muted">June</span><br />WSI welcomes James Stuber as Design Engineer. </p>
+	<p><span class="text-muted">April</span><br />Woodruff presents a short History of Fusion to the Earth and Space Sciences department, University of Washington. </p>
+	<p><span class="text-muted">April</span><br />First Look Forum finalist presentation </p>
+	<p><span class="text-muted">March</span><br />WSI has made it through to the top 12 in the Northwest Entrepreneur Network’s ‘First Look Forum’, and will be presenting to a wide investment community on April 12th. </p>
+
+	<h3>2010</h3>
+	<p><span class="text-muted">July</span><br />WSI welcomes Chris Raastad, James Stuber, and Audrey Knight as summer interns.</p>
+	<p><span class="text-muted">June</span><br />WSI welcomes James Stuber and Sara Bucht as this years NASA Space Grant Summer interns.</p>
+	<p><span class="text-muted">February</span><br />Fusion internal combustion engine presented at the Innovative Confinement Concepts workshop, Princeton Plasma Physics Laboratory, Princeton, New Jersey </p>
+	<p><span class="text-muted">February</span><br />Fusion engine patent filed </p>
+
+
+	<h3>2009</h3>
+	<p><span class="text-muted">April</span><br />Design work for the Spheromak Turbulence Physics Experiment (STPX) begins.
+		<br /><a href="img/Full.jpg"><img src="img/Full-300x124.jpg" alt="STPX design"></a></p>
+	<p><span class="text-muted"> March </span><br />FAMU places order for world’s largest Spheromak device, as a Phase III of our 2007 DOE Phase II SBIR.</p>
+
+
+	<h3>2008</h3>
+	<p><span class="text-muted">November 17-21</span><br />Results of compression experiment presented at the 50th Annual Meeting of the Division of Plasma Physics (DPP), Dallas, Texas</p>
+	<p><span class="text-muted">November 6</span><br />The <a href="http://science.energy.gov/~/media/fes/fesac/pdf/2008/Toroidal_alternates_panel_report.pdf">report</a> of the Fusion Energy Sciences Advisory Committee Toroidal Alternates Panel (FESAC TAP) released – WSI had been major contributor to the CT section of this report.</p>
+	<p><span class="text-muted">September</span><br />WSI welcomes Jennifer Baerny as Research Assistant to help with work on both Phase II SBIR projects and other private projects.</p>
+	<p><span class="text-muted">August</span><br />1st plasmas in the device built for SBIR on compressional heating and current drive.
+		<br /><a href="img/1stplasma.jpg"><img src="img/1stplasma.jpg" height="300px"></a></p>
+	<p><span class="text-muted">June</span><br />WSI welcomes Jennifer Baerny as WSI summer intern through the Washington Space Grant Program.</p>
+	<p><span class="text-muted">June</span><br />2nd DOE Phase II SBIR won! Analytic and Computational Study of the Adiabatic Compression of Simply Connected Magnetized Targets.</p>
+
+
+	<h3>2007</h3>
+	<p><span class="text-muted">December 4</span><br /> Summary of the <a href="http://fire.pppl.gov/fpa_annual_meet.html#2007">2007 Innovative Confinement Concepts workshop</a> presented at the Fusion Power Associates meeting, Capitol Hill, Washington DC.</p>
+	<p><span class="text-muted">June</span><br />1st DOE Phase II SBIR won! Compression of Compact Tori for Current Drive and Heating.</p>
+	<p><span class="text-muted">June</span><br />Presentation of results from SBIRs at the <a href="http://www.ece.unm.edu/ppps2007/">2007 IEEE Pulsed Power and Plasma Science Conference</a>, Albuquerque, New Mexico</p>
+	<p><span class="text-muted">June</span><br />1st order for HPC received.</p>
+	<p><span class="text-muted">June</span><br />Woodruff Scientific Lab opens in Ballard.</p>
+	<p><span class="text-muted">February</span><br />2nd DOE Phase I SBIR won! Analytic and Computational Study of the Adiabatic Compression of Simply Connected Magnetized Targets. </p>
+	<p><span class="text-muted">February 12 - 14</span><br />Woodruff chairs ‘New Concepts’ session at the 2007 Innovative Confinement Concepts Workshop, College Park, Maryland</p>
+
+
+	<h3>2006</h3>
+	<p><span class="text-muted">November</span><br />1st results of SBIR presented at the American Physical Society Division of Plasma Physics conference, Philadelphia, Pennsylvania</p>
+	<p><span class="text-muted">June</span><br />Work starts on the Phase I SBIR.</p>
+	<p><span class="text-muted">February</span><br />1st DOE Phase I SBIR won! High Energy Collisions of Field-Reversed Configurations.</p>
+	<p><span class="text-muted">February 13-16</span><br /> Woodruff chairs the ‘New Concepts’ session of the <a href="http://www.iccworkshops.org/icc2006">Innovative Confinement Concepts Workshop 2006 Austin, Texas</a></p>
+
+
+	<h3>2005</h3>
+	<p><span class="text-muted">October: </span><br />47th Annual Meeting of the American Physical Society Division of Plasma Physics – summary presentation on innovative confinement concepts.</p>
+	<p><span class="text-muted">May 6:</span><br /> Woodruff Scientific founded and registered</p>
+	<p><span class="text-muted"></span><br /></p>
+</div>
 
     </div> <!-- /container -->
     </div> <!-- /wrap -->
