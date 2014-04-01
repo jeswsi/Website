9616cf6926d994c7e3ae3e4f20d579bcda816240
jeswsi <james@woodruffscientific.com>
2014-04-01 11:25:59 -0700

    added more info to labview page, allow download of labview code

--- labview.php
+++ labview.php
@@ -28,20 +28,32 @@
 	<p>WSI uses National Instruments <a href="http://www.ni.com/labview/">LabVIEW</a> for experiment control.
 	<p>Our software includes:
 		<ul>
-			<li>Integration with <a href="safety">saftey interlocks</a></li>
-			<li>Control of charging &amp; firing of banks</li>
-			<li>Read in of data channels and export of data</li>
+			<li>Integration with <a href="safety">safety interlocks</a></li>
+			<li>Control of charging &amp; firing of banks (PXI Star high-performance trigger signaling &lt;1ns inter-module delay)</li>
+			<li>Read in of data channels and export of data (csv, MDS+, etc.)</li>
 			<li>Real time abort</li>
 			<li>Expandable, customizable, and compatible with NI equipment</li>
 		</ul>
-		Typical use is for charging of banks and timing of firing, acquiring data on diagnostic channels. All input controlled through front panel.
+		Typical use is for charging of banks and timing of firing, acquiring data on diagnostic channels. All input controlled through front panel. See <a href="daq">Data Acquisition page</a> for example hardware setups for use with Experiment Management System.</p>
+		<p>A basic version of the Experiment Management System is available free of charge:
 	</p>
+	<p><a class="btn btn-lg btn-primary" onclick="agree()" role="button">Download Basic Experiment Management System &raquo;</a></p>
+<br />
+	<p>If you need further assistance using or configuring the Experiment Management system, let us know. WSI can also modify the experiment management system to better suit your lab. Let us know what modifications you need.</p>
 	<p><a class="btn btn-lg btn-primary" href="mailto: sales@woodruffscientific.com?subject=Experiment management system RFQ &#038;body=How%20many%20banks:%0D%0A%0D%0AHow%20many%20acquisition%20channels:%0D%0A%0D%0AShort%20description%20of%20the%20application:%0D%0A%0D%0ANeeded%20by(Date):%0D%0A%0D%0AContact(Name/email/phone/fax/etc):" role="button">Get a Quote &raquo;</a></p>
 
 
     </div> <!-- /container -->
     </div> <!-- /wrap -->
+    <script>
+    function agree()
+    {
+    	if(confirm("By downloading this software, you agree to the following terms:\n\nThe software is provided 'as is', without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.")) {
+    		window.location.href="files/labview_experiment_management_system.zip"
+    	}
 
+    }
+    </script>
 	<?php
 	$footer = file_get_contents('footer.html');
 	echo $footer;	
