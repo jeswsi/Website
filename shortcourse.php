072c8985f18802b84317e5f9489655e13c5a8dc1
jeswsi <james@woodruffscientific.com>
2016-06-13 10:00:53 -0700

    updated dates and topics for 2016 bootcamp

--- shortcourse.php
+++ shortcourse.php
@@ -24,8 +24,9 @@
 	</ol>
 	<h1>Scientific Computing Bootcamp<br/>
 		<small>with emphasis on application to plasma physics and nuclear fusion</small></h1>
-	<blockquote>September 14-18, 2015<br/>4000 Aurora Ave N. Suites 5 &amp; 6<br />
-		Seattle, WA 98103</blockquote>
+	<blockquote>September 19-23, 2016<br/>
+		Location to be announced (Seattle Area)
+		</blockquote>
 
 	<p>Be introduced to the tools that plasma physics simulationists use regularly in addressing contemporary 
 issues in scientific computing and some of today's most pressing challenges. Relevant materials will be provided.</p>
@@ -37,12 +38,11 @@ interest in pursuing high performance computing in their careers.</p>
 		<ul>
 			<li>Grand challenges in computational plasma physics</li>
 			<li>Overall process of computational simulation</li>
-			<li>Historical context</li>
 			<li>Introduction to plasma physics research &amp; development codes</li>
 			<li>Compiling and debugging, including Make, C++, and FORTRAN</li>
 			<li>High performance computing and best practices</li>
 			<li>Visualization and post-processing tools, including Octave and Python</li>
-			<li>Partial differential equations, the finite element method</li>
+			<li>The finite element method</li>
 			<li>Verification and validation in scientific computing</li>
 			<li>Contemporary issues</li>
 			<li>Exascale computing</li>
@@ -50,11 +50,11 @@ interest in pursuing high performance computing in their careers.</p>
 	</p>
 	<br>
 
-	<h4>This year's course was a great success, thanks to all the students, guest speakers, and instructors. Look to this page for future bootcamps.</h4>
-	<h4><a href="files/2015-Bootcamp-Summary.pdf">2015 Bootcamp Summary (pdf)</a></h4>
-<!-- 
+<!-- 	<h4>This year's course was a great success, thanks to all the students, guest speakers, and instructors. Look to this page for future bootcamps.</h4>
+ -->	<h4><a href="files/2015-Bootcamp-Summary.pdf">2015 Bootcamp Summary (pdf)</a></h4>
+
 <p class="center"><a class="btn btn-lg btn-primary" href="apply" role="button">Apply Now &raquo;</a></p><br />
-<p class="center"><big>Applications accepted through <strong>Friday, May 29</strong></big></p> -->
+<p class="center"><big>Applications accepted through <strong>Friday, June 24</strong></big></p>
     </div> <!-- /container -->
     </div> <!-- /wrap -->
 
