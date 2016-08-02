0cea6483f87fd4308d7e85c159acfdc67943cd30
jeswsi <james@woodruffscientific.com>
2016-08-02 09:30:31 -0700

    Update
    add links

--- systems.php
+++ systems.php
@@ -29,6 +29,21 @@
 		<blockquote><a href="costing.html">Costing References</a></blockquote>
 
 
+		<h3>Systems Analysis Papers by Woodruff Scientific</h3>
+
+			
+		<p><a href="http://www.sciencedirect.com/science/article/pii/S0920379614005997">Fusion Engineering and Design: Cost sensitivity analysis for a 100 MWe modular power plant and fusion neutron source</a></p>
+
+		<p><a href="http://link.springer.com/article/10.1007/s10894-010-9303-1">Journal of Fusion Energy: Why Compact Tori for Fusion?</a></p>
+
+		<p><a href="http://link.springer.com/article/10.1007%2Fs10894-011-9472-6">Journal of Fusion Energy: Path to Market for Compact Modular Fusion Power Cores</a></p>
+
+		<p><a href="https://www.burningplasma.org/web/ReNeW/whitepapers/5-32%20ThemeIV+CT.pdf">Burning Plasma Org: Harnessing fusion power: how does CT research contribute to this discussion?</a></p>
+
+		<p><a href="http://fire.pppl.gov/FESAC_WP_CT_market_Woodruff.pdf">FESAC: The Path to Market for Compact Modular Fusion Power Cores</a></p>
+
+
+
     </div> <!-- /container -->
     </div> <!-- /wrap -->
 
