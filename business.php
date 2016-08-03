5f1fefcaa8338ddc6473c0f085a10fb8a2e71347
jeswsi <james@woodruffscientific.com>
2016-08-03 10:14:25 -0700

    Add
    Add Will to page and update Morgan and Simon's link to linkedin

--- business.php
+++ business.php
@@ -28,7 +28,7 @@
 
 	<img src="img/Headshots/simon.jpg" alt="Image of Simon Woodruff" style="width:128px;" align="left" Hspace="10">
 
-			<h3><strong><a href="http://www.researchgate.net/profile/Simon_Woodruff/">Dr. Simon Woodruff</a>
+			<h3><strong><a href="https://www.linkedin.com/in/simonwoodruff">Dr. Simon Woodruff</a>
 </strong><span class="text-muted">President & CEO</span></h3>
 <p>Since 2005, Woodruff has lead the company through initial start-up phases.  He has established advisory boards consisting of  energy industry and scientific leaders, building the company from the ground up and opening the laboratory in Ballard, managing contracts from DOE, national laboratories and universities.  In the last year, under his direction, WSI beat 40 other companies for the opportunity to present to investors for the first time at the Northwest Entrepreneur Network&#8217;s <a href="http://www.nwen.org/index?option=com_events&amp;Itemid=15&amp;id=526">&#8216;First Look Forum&#8217;</a>, and subsequently was able to meet with leading venture capitalists.  He developed the strategy for the business over several years taking in advice at annual strategic planning meetings, inviting local experts and business leaders, following leadership during his employment in the Lawrence Livermore National Laboratory Fusion Energy Program.  Woodruff has ensure that the company maintains strategic partnerships with many organizations locally, nationally and internationally, and aims to continue to grow the business.</p>
 
@@ -37,7 +37,7 @@
 
 <img src="img/Headshots/morgan.jpg" alt="Image of Morgan Quinley" style="width:128px;" align="left" Hspace="10">
 
-<h3><strong><a href="https://www.researchgate.net/profile/Morgan_Quinley/">Morgan Quinley</a></strong><span class="text-muted"> COO</span></h3>
+<h3><strong><a href="https://www.linkedin.com/in/morgan-quinley-b06a9013">Morgan Quinley</a></strong><span class="text-muted"> COO</span></h3>
 
 <p>Morgan brings many years of experience gained working in industrial settings to his role as Chief Operations Officer.  Morgan earned a bachelor's degree in aerospace engineering from Saint Louis University. He then worked for a few years in the field of nuclear medicine at a large business before starting graduate school in plasma science at the University of Washington, where he obtained his MSc.  Morgan currently splits his time between business strategy, management and planning and scientific duties as Diagnostic Engineer.</p>
 
@@ -54,19 +54,11 @@
 <br></br>
 
 
-
-
-
 <h1>Accounting</h1>
 
 <h3><strong><a href="http://www.greenwoodcpa.com/AmandaORourke.php">Amanda O'Rourke</a></strong>
 <span class="text-muted">CPA, Greenwood Ohlund and Co. LLP</span></h3>
 <p>Amanda is a licensed CPA who specializes in nonprofit services including attestation engagements, tax preparation and financial management. She has been with Greenwood, Ohlund & Co., LLP since 2009 and brings a diverse background in public accounting, as well as financial management in the nonprofit sector and the construction industry. </p>
-
-<br></br>
-
-
-
 
 
 
@@ -79,12 +71,14 @@
 <h3><strong><a href="http://www.linkedin.com/in/johanssonlars">Lars Johansson</a></strong> <span class="text-muted">Business advisory board member</span></h3>
 <p>Lars is the co-chair of the Northwest Energy Angels and an active Cleantech angel investor through his company Energethic, LLC. He has invested in early stage companies since the late 90s with an exclusive focus on Cleantech in recent years. He invests primarily in USA/Canada but also in his native country of Sweden. His background is in IT where he was an executive at Microsoft for a combined 16 years. Lars joined Microsoft in Sweden and was responsible for the Nordic Enterprise business when in 1993 he relocated to Redmond to lead the development of the company’s initial volume licensing programs. Most recently he was responsible for the Worldwide System Builder business.  He is on the board of Groundwire (formerly ONE/Northwest), an active supporter of Climate Solutions and a long-time partner with Social Venture Partners, having served on its Environmental Grant Committee. Lars has a Business degree from the University of Uppsala and an Engineering degree from Älvkullegymnasiet in Karlstad, both in Sweden.</p>
 
+
+<h3><strong><a href="https://uk.linkedin.com/in/william-woodruff-6a6b6231">William Woodruff</a></strong> <span class="text-muted">Business advisory board member</span></h3>
+<p>William is a Senior Associate at PricewaterhouseCoopers in the Channel Islands, UK. He qualified as a Charted Accountant working in London with a client base primarily consisting of SMEs including those benefiting from Research and Development tax relief. After qualifying William moved to Guernsey to further his specialism within the Financial Services industry. Since then he has audited and advised LSE Premium Listed funds, Private Equity funds, and Venture Capitalist funds. His expertise includes, Financial Reporting, Management Reporting, and Financial & Strategic Analysis. William's academic background includes a bachelor's degree in Physics and a Master's degree in Analytical Science from the University of Warwick.</p>
+
+
+
 <h3><strong><a href="http://uk.linkedin.com/pub/matt-woodruff/1/780/904/">Matthew Woodruff</a></strong> <span class="text-muted">Business advisory board member</span></h3>
 <p>Prior to joining Lookred® as a director Matthew spent five years as a Senior Project Manager with Microsoft Consulting Services in the UK. During his time at Microsoft, Matthew gained extensive experience of managing high profile public sector programmes ranging from short specialist consulting engagements to multi-year, multi-million pound projects with large development teams and multiple partner organisations. Notable projects included management of the application development of the Government Gateway and working with multiple regional Police Authorities – leading programmes of work delivering infrastructure and application platform projects across identity and access management, messaging and collaboration and business intelligence. Matthew first became involved in education sector work and met Chris managing the Microsoft BSF Showcase, the success of which led to securing further funding and the publication of the Microsoft BSF Technology Guidance and successful technology showcases for both Further Education, and Higher Education sectors.</p>
-
-<h3><strong><a href="http://marstonconsulting.net/">Dr. Theodore Marston</a> </strong><span class="text-muted">Business advisory board member </span></h3>
-<p>Upon retirement from the Electric Power Research Institute (EPRI) as its Chief Technology Officer, Dr. Marston established Marston Consulting in June of 2006. This firm is dedicated to the innovation, development, demonstration and deployment of new technology to address two key issues facing developed and developing countries in the 21st century: energy independence and management of global climate change. His clients include venture capitalists, commercial and energy companies, R&amp;D organizations, and U.S. and international national laboratories.</p>
-<p>Previously, as CTO of EPRI, he directed multi-hundred million dollar, international science and technology programs to improve the generation, transmission, distribution and utilization of electricity and reduce the associated environmental risks. Earlier, he led a large international program to develop utility requirements for advanced nuclear reactors, design certification for advanced light water reactors, first-of-a-kind engineering and siting of nuclear reactors. In addition to his nuclear experience, he developed international, independent, fossil-fueled power generation projects. Ted has over 30 years of global experience in the assessment and management of risk in a broad range of industrial facilities, including nuclear and conventional power plants, refineries, chemical plants, railroads, and defense facilities. He received his PhD in Mechanical Engineering from the University of Michigan in 1972 and is a Fellow of the American Society of Mechanical Engineers.</p>
 
 <h3><strong>Raymond Williams</strong> <span class="text-muted">Business advisory board member</span></h3>
 <p>Raymond A. Williams, Jr. received his undergraduate degree in Industrial Management from the University of Washington and his MBA in Investments and Corporate Finance from the New York University, Graduate School of Business. He currently serves as President, CEO and Chairman of the Board for Xbridge Systems.</p>
