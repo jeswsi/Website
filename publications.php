e2ac0517643b89c17b6b60a07ec6900e0e184b49
jeswsi <james@woodruffscientific.com>
2016-11-09 16:19:30 -0800

    Publications page
    created

---
+++ publications.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<?php
+	$head = file_get_contents('header.html');
+	echo $head;	
+	?>
+  <meta name="description" content="">
+  <title>TITLE | Woodruff Scientific, Inc.</title>
+  </head>
+
+  <body>
+<?php $googleTagManager = file_get_contents('googleTagManager.html'); echo $googleTagManager; ?>
+    <div id="wrap">
+    <div class="container">
+
+	<?php
+	$navbar = file_get_contents('navbar.html');
+	echo $navbar;	
+	?>	
+	<ol class="breadcrumb">
+  		<li><a href="index">Home</a></li>
+ 	 	<li><a href="#">Subsection</a></li>
+  		<li class="active">Current Page</li>
+	</ol>
+	<h1>Title</h1>
+	<p>Paragraph</p>
+
+    </div> <!-- /container -->
+    </div> <!-- /wrap -->
+
+	<?php
+	$footer = file_get_contents('footer.html');
+	echo $footer;	
+	?>	   
+  </body>
+</html>
