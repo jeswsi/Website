d7e52e71308023909046af8b200db5f6ebed236d
jeswsi <james@woodruffscientific.com>
2016-11-14 08:02:43 -0800

    update

--- publications.php
+++ publications.php
@@ -5,8 +5,8 @@
 	$head = file_get_contents('header.html');
 	echo $head;	
 	?>
-  <meta name="description" content="Articles and presentations created by WSI employees">
-  <title>Publications | Woodruff Scientific, Inc.</title>
+  <meta name="description" content="">
+  <title>PUBLICATIONS | Woodruff Scientific, Inc.</title>
   </head>
 
   <body>
@@ -21,8 +21,22 @@
 	<ol class="breadcrumb">
   		<li><a href="index">Home</a></li>
  	 	<li><a href="#">Company</a></li>
-  		<li class="active">Publications Page</li>
+  		<li class="active">Publications</li>
 	</ol>
+	
+	<h1>APS Conference Posters</h1>
+	<p>Come checkout some of our research and discoveries! Please view list of posters below:</p>
+	<ul>
+		<li><a href="http://www.woodruffscientific.com/temp/APS2016/Rivera-APS-2016.pdf">Vacuum Compatibility of Laser-Sintered Metals</a></li>
+		<li><a href="http://www.woodruffscientific.com/temp/APS2016/Sieck-APS-2016.pdf">Engineering Design Point for a 1MW Fusion Neutron Source</a></li>
+		<li><a href="http://www.woodruffscientific.com/temp/APS2016/Woodruff-APS-2016.pdf">Physics Design Point for a 1MW Fusion Neutron Source</a></li>	
+		<li><a href="http://www.woodruffscientific.com/temp/APS2016/Stuber-APS-2016.pdf">Optimized and Automated Design of Plasma Diagnostics for Additive Manufacture</a></li>
+		<li><a href="http://www.woodruffscientific.com/temp/APS2016/OBryan-APS-2016.pdf">Numerical Investigation of Design and Operating Parameters on CHI Spheromak Performance</a></li>
+		<li><a href="http://www.woodruffscientific.com/temp/APS2016/Quinley-APS-2016.pdf">Additive Manufacture (3D Printing) of Plasma Diagnotic Components and Assemblies for Fusion Experiments</a></li>
+	</ul>
+
+
+
 
 	<h1>Articles</h1>
 	<p>Come checkout some of our research and discoveries! Please view list of articles below:</p>
@@ -31,60 +45,64 @@
 		<li><a href="http://woodruffscientific.com/pdf/HTPD%20Poster.pdf">HTPD Poster</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/IEEE%20Poster%202016.pdf">IEEE Poster</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/Woodruff-Testimonial.pdf">Testimonial</a></li>
-				
 	</ul>
 
 
-	<h1>Circuts</h1>
+	<h1>Circuits</h1>
 	<p>Come checkout some of our work on circuts! Please view list of images below:</p>
 	<ul>
-		<li><a href="http://woodruffscientific.com/pdf/Coil1.1-8mF-5O-0.3089H.pdf">Image of Coil 1, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil1.1-8mF-5O-0.3089Ht.pdf">Image of Coil 1, Volts and Currents</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil2.1-2mF-32mO-0.021mH.pdf">Image of Coil 2, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil2.1-2mF-32mO-21mHt.pdf">Image of Coil 2, Volts and Currents</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil3.1-2mF-0.3mO-3uH.pdf">Image of Coil 3, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil3.1-2mF-0.3mO-3uHt.pdf">Image of Coil 3, Volts and Currents</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil4.1-2mF-0.16O-9.3mH1.pdf">Image of Coil 4, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil4.1-2mF-0.16O-9.3mHt.pdf">Image of Coil 4, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil5.1-2mF-74uO-3.2uH.pdf">Image of Coil 5, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil5.1-2mF-74uO-3.2uHt.pdf">Image of Coil 5, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil6.1-2mF-0.7mO-1.9uH.pdf">Image of Coil 6, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil6.1-2mF-0.7mO-1.9uHt.pdf">Image of Coil 6, Volts and Current</a></li>		
-		<li><a href="http://woodruffscientific.com/pdf/Coil7.1-2mF-25uO-0.39uH.pdf">Image of Coil 7, Example Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coil7.1-2mF-25uO-0.39uHt.pdf">Image of Coil 7, Volts and Current</a></li>		
+		<li><a href="http://woodruffscientific.com/pdf/Coil1.1-8mF-5O-0.3089H.pdf">Coil 1, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil1.1-8mF-5O-0.3089Ht.pdf">Coil 1, Volts and Currents</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil2.1-2mF-32mO-0.021mH.pdf">Coil 2, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil2.1-2mF-32mO-21mHt.pdf">Coil 2, Volts and Currents</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil3.1-2mF-0.3mO-3uH.pdf">Coil 3, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil3.1-2mF-0.3mO-3uHt.pdf">Coil 3, Volts and Currents</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil4.1-2mF-0.16O-9.3mH1.pdf">Coil 4, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil4.1-2mF-0.16O-9.3mHt.pdf">Coil 4, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil5.1-2mF-74uO-3.2uH.pdf">Coil 5, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil5.1-2mF-74uO-3.2uHt.pdf">Coil 5, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil6.1-2mF-0.7mO-1.9uH.pdf">Coil 6, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil6.1-2mF-0.7mO-1.9uHt.pdf">Coil 6, Volts and Current</a></li>		
+		<li><a href="http://woodruffscientific.com/pdf/Coil7.1-2mF-25uO-0.39uH.pdf">Coil 7, Example Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coil7.1-2mF-25uO-0.39uHt.pdf">Coil 7, Volts and Current</a></li>		
 	</ul>
 
 <h1>Coilsets</h1>
 	<p>Come checkout some of our work on coilsets! Please view list of coilsets below:</p>
 	<ul>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF1andPF5.pdf">Coilset 1 Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF1andPF5t.pdf">Image of Coilset 1, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF2andPF61.pdf">Coilset 1 Circuit 2</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF2andPF6t.pdf">Image of Coilset 1, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF3andPF7.pdf">Coilset 1 Circuit 3</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF3andPF7t.pdf">Image of Coilset 1, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF4andPF8.pdf">Coilset 1 Circuit 4</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF4andPF8t.pdf">Image of Coilset 1, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF1andPF4.pdf">Coilset 2 Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF1andPF4t.pdf">Image of Coilset 2, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF2andPF3.pdf">Coilset 2 Circuit 2</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF2andPF3t.pdf">Image of Coilset 2, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF1andPF6.pdf">Coilset 3 Circuit 1</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF1andPF6t.pdf">Image of Coilset 3, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF2andPF7.pdf">Coilset 3 Circuit 2</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF2andPF7t.pdf">Image of Coilset 3, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF3andPF8.pdf">Coilset 3 Circuit 3</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF3andPF8t.pdf">Image of Coilset 3, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF4andPF9.pdf">Coilset 3 Circuit 4</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF4andPF9t.pdf">Image of Coilset 3, Volts and Current</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF5andPF10.pdf">Coilset 3 Circuit 5</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF5andPF10t.pdf">Image of Coilset 3, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF1andPF5.pdf">Coilset 1, Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF1andPF5t.pdf">Coilset 1, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF2andPF61.pdf">Coilset 1, Circuit 2</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF2andPF6t.pdf">Coilset 1, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF3andPF7.pdf">Coilset 1, Circuit 3</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF3andPF7t.pdf">Coilset 1, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF4andPF8.pdf">Coilset 1, Circuit 4</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset1PF4andPF8t.pdf">Coilset 1, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF1andPF4.pdf">Coilset 2, Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF1andPF4t.pdf">Coilset 2, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF2andPF3.pdf">Coilset 2, Circuit 2</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset2PF2andPF3t.pdf">Coilset 2, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF1andPF6.pdf">Coilset 3, Circuit 1</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF1andPF6t.pdf">Coilset 3, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF2andPF7.pdf">Coilset 3, Circuit 2</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF2andPF7t.pdf">Coilset 3, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF3andPF8.pdf">Coilset 3, Circuit 3</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF3andPF8t.pdf">Coilset 3, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF4andPF9.pdf">Coilset 3, Circuit 4</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF4andPF9t.pdf">Coilset 3, Volts and Current</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF5andPF10.pdf">Coilset 3, Circuit 5</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/Coilset3PF5andPF10t.pdf">Coilset 3, Volts and Current</a></li>
 
 	</ul>
 
 	<h1>Band Design Points</h1>
 	<p>Come checkout some of our work on Bank Design Points! Please view list of images below:</p>
 	<ul>
+
+
+		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/TF_Modelsheet.pdf">TF Capacitor Bank</a></li>
+		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/TF_spec_Rev2A.pdf">Specification for the Toroidal Field Bank Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE, March 11, 2016</a></li>		
+		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/TF_spec_day1_Rev1A.pdf">Specification for the Toroidal Field Bank Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE - Day 1, March 11, 2016</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/Charge_spec_Rev2A.pdf">Specification for the Charge Supplies Capacitor Charge/Discharge Pwer Suppky (CCDPS) for FLARE, March 11, 2016</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/DAQ_spec_Rev1A.pdf">Specification for the CCDPS DAQ?Control Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE, March 8, 2016</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/DC_spec_Rev2A.pdf">Specification for the Driver Coil Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE, March 10, 2016</a></li>
@@ -95,12 +113,9 @@
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/OH_spec_Rev2A.pdf">Specification for the Ohmic Heating Module Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE, March 11, 2016</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/PF_spec_Rev3A.pdf">Specification for the Poloidal Field Bank Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE, March 11, 2016</a></li>
 		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/PF_spec_day1_Rev1A.pdf">Specification for the Poloidal Field Bank Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE - Day 1, March 11, 2016</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/TF_Modelsheet.pdf">TF Capacitor Bank</a></li>
-		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/TF_spec_Rev2A.pdf">Specification for the Toroidal Field Bank Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE, March 11, 2016</a></li>		
-		<li><a href="http://woodruffscientific.com/pdf/bankdesignpoints/TF_spec_day1_Rev1A.pdf">Specification for the Toroidal Field Bank Modules Capacitor Charge/Discharge Power Supply (CCDPS) for FLARE - Day 1, March 11, 2016</a></li>
+		
 		
 	</ul>
-
 
     </div> <!-- /container -->
     </div> <!-- /wrap -->
